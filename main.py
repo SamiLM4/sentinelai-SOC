f356d034d240be60fd4b0d00027b31acf4eedf3e
Murilo Lima <mglima.ntz@gmail.com>
2026-08-24 16:50:51 -0300

    CORS DEPLOY

--- main.py
+++ main.py
@@ -36,7 +36,10 @@ def criar_tabelas():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://sentinelai-dashboard.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
